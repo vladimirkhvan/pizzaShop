80f793332c97e7585ac668b561c24e512c869253
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-07-03 13:36:07 +0500

    moved fetch pizzas to redux using createAsyncThunk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import qs from 'qs';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,19 +7,19 @@ import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import PizzaSkeleton from '../components/PizzaSkeleton';
 import Pagination from '../components/Pagination';
+import InfoBlock from '../components/InfoBlock';
 
 import { SearchContext } from '../context/SearchContext';
 
 import { useSelector, useDispatch } from 'react-redux';
 
 import { setFilters } from '../redux/slices/filterSlice';
+import { fetchPizza } from '../redux/slices/pizzaSlice';
 
 const Home = () => {
-    const [pizzas, setPizzas] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
 
-    const { categoryId, sortSelector } = useSelector((state) => state.filter);
-    const pageIndex = useSelector((state) => state.filter.pageIndex);
+    const { categoryId, sortSelector, pageIndex } = useSelector((state) => state.filter);
+    const { items: pizzas, status } = useSelector((state) => state.pizza);
 
     const { searchValue } = React.useContext(SearchContext);
 
@@ -45,16 +44,7 @@ const Home = () => {
             const category = categoryId ? `&category=${categoryId}` : '';
             const search = searchValue ? `&title=${searchValue}` : '';
 
-            setIsLoading(true);
-
-            axios
-                .get(
-                    `https://62b7554a691dcea2733d6cff.mockapi.io/items?page=${pageIndex}&limit=4&sortBy=${sort}${search}&order=${order}${category}`,
-                )
-                .then((res) => {
-                    setPizzas(res.data);
-                    setIsLoading(false);
-                });
+            dispatch(fetchPizza({ sort, order, category, search, pageIndex }));
         }
 
         isSearch.current = false;
@@ -81,9 +71,13 @@ const Home = () => {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-                {isLoading
-                    ? [...Array(4)].map((item, index) => <PizzaSkeleton key={index} />)
-                    : pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
+                {status === 'rejected' ? (
+                    <InfoBlock />
+                ) : status === 'pending' ? (
+                    [...Array(4)].map((item, index) => <PizzaSkeleton key={index} />)
+                ) : (
+                    pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)
+                )}
             </div>
             <Pagination numberOfPages={3} />
         </>
