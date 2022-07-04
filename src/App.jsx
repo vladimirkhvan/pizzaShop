2a4a349ba3b263032c99765d80aa784aab0af9a9
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-07-04 23:33:10 +0500

    moved main layout to own component

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,23 @@ import { Routes, Route } from 'react-router-dom';
 
 import './scss/app.scss';
 
-import Header from './components/Header';
+import MainLayout from './layout/MainLayout';
 
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
 import PizzaItem from './components/PizzaItem';
 
-import { SearchContext } from './context/SearchContext';
-
 function App() {
     return (
-        <div className="wrapper">
-            <Header />
-
-            <div className="content">
-                <div className="container">
-                    <Routes>
-                        <Route path="" element={<Home />} />
-                        <Route path="/pizza/:id" element={<PizzaItem />} />
-                        <Route path="cart" element={<Cart />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </div>
-            </div>
-        </div>
+        <Routes>
+            <Route path="/" element={<MainLayout />}>
+                <Route path="" element={<Home />} />
+                <Route path="pizza/:id" element={<PizzaItem />} />
+                <Route path="cart" element={<Cart />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
     );
 }
 
